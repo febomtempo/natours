88cfe39741397061e4121ba258387d3cc4416833
unknown <febomtempo@hotmail.com>
2023-02-14 16:26:10 -0300

    feat: implement MongoDB with mongoose

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose.set('strictQuery', true);
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
