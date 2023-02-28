87c3ba2573b72ad965fb1bc2455b09bda67106bb
unknown <febomtempo@hotmail.com>
2023-02-28 13:34:40 -0300

    feat: extending base template with blocks

--- app.js
+++ app.js
@@ -69,7 +69,22 @@ app.use((req, res, next) => {
 
 // 2) ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Felipe',
+  });
+});
+
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
