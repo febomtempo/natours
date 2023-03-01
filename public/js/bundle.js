3dcf0ad4d703c597148b44e264dd7abd9c0e17bc
unknown <febomtempo@hotmail.com>
2023-03-01 20:55:57 -0300

    feat: image uploads and multer config

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -21578,7 +21578,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56976" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51748" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
