e28dab106c0e87a8246849009c21d042c05a4179
unknown <febomtempo@hotmail.com>
2023-03-01 19:28:55 -0300

    feat: updating user data

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -21479,7 +21479,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56779" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56976" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
